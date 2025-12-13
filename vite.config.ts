20de1bdd94c37bcbb6de0722605a60e248e576c2
Corban Thompson <corbant25@gmail.com>
2025-12-13 11:58:51 -0700

    feat: add pwa functionality

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,47 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'favicon-32x32.png',
+        'favicon-16x16.png',
+        'pwa-192x192.png',
+        'pwa-512x512.png'
+      ],
+      manifest: {
+        name: 'Sweeepstakes',
+        short_name: 'Sweeepstakes',
+        description: 'From mess to success',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      },
+      registerType: 'autoUpdate'
+    })
+  ],
   server: {
     proxy: {
       '/api': 'http://localhost:4000',
